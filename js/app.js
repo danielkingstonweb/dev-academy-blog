06d2b6fdda99f8ec1ad216e23629c5dbcc89aa3d
Daniel Kingston <dnlkingston75@gmail.com>
2023-09-10 14:48:38 +1200

    learning Plan Layed out

---
+++ js/app.js
@@ -0,0 +1,25 @@
+console.log('connected');
+
+// Learning Section Hide Show Variables
+
+const learningOne = document.querySelector('#learning-block-1');
+const learningTwo = document.querySelector('#learning-block-2');
+const learningThree = document.querySelector('#learning-block-3');
+const learningFour = document.querySelector('#learning-block-4');
+const learningFive = document.querySelector('#learning-block-5');
+const learningSix = document.querySelector('#learning-block-6');
+const learningSeven = document.querySelector('#learning-block-7');
+
+// Learning Section btn Variables
+
+const learningBtnOne = document.querySelector('#learning-btn-1');
+const learningBtnTwo = document.querySelector('#learning-btn-2');
+const learningBtnThree = document.querySelector('#learning-btn-3');
+const learningBtnFour = document.querySelector('#learning-btn-4');
+const learningBtnFive = document.querySelector('#learning-btn-5');
+const learningBtnSix = document.querySelector('#learning-btn-6');
+const learningBtnSeven = document.querySelector('#learning-btn-7');
+
+function hideShow(){
+  
+}
