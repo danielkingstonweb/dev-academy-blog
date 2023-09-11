b0dcd7b192d838c4adb926b2b82a536a3ba80135
Daniel Kingston <dnlkingston75@gmail.com>
2023-09-11 13:47:10 +1200

    Deleted unused variables

--- js/app.js
+++ js/app.js
@@ -1,24 +1,6 @@
 console.log('connected')
 
-// Learning Section Hide Show Variables
-
-const learningOne = document.querySelector('#learning-block-1')
-const learningTwo = document.querySelector('#learning-block-2')
-const learningThree = document.querySelector('#learning-block-3')
-const learningFour = document.querySelector('#learning-block-4')
-const learningFive = document.querySelector('#learning-block-5')
-const learningSix = document.querySelector('#learning-block-6')
-const learningSeven = document.querySelector('#learning-block-7')
-
-// Learning Section btn Variables
-
-const learningBtnOne = document.querySelector('#learning-btn-1')
-const learningBtnTwo = document.querySelector('#learning-btn-2')
-const learningBtnThree = document.querySelector('#learning-btn-3')
-const learningBtnFour = document.querySelector('#learning-btn-4')
-const learningBtnFive = document.querySelector('#learning-btn-5')
-const learningBtnSix = document.querySelector('#learning-btn-6')
-const learningBtnSeven = document.querySelector('#learning-btn-7')
+// Hide Show Learning Plan Function Begins
 
 let activeSection = 'learning-block-1'
 
@@ -55,6 +37,8 @@ function hideShowToggle(sectionId) {
     sectionId
   )
   activeSection = sectionId
-  
-  console.log(activeSection);
+
+  console.log(activeSection)
 }
+
+// Hide Show Learning Plan Function Ends
